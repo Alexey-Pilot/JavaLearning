7e41f68bba561b9ea7b3748506215b630c4c7d4e
Alexey-Pilot <alexragnarek@gmail.com>
2023-04-25 20:56:46 +0500

    corr

--- src/HW_4/EX_2.java
+++ src/HW_4/EX_2.java
@@ -1,6 +1,14 @@
 package HW_4;
 import java.util.LinkedList;
+final class DequeueL{
+    public int firstElem;
+    public LinkedList<Integer> lList;
+    public DequeueL(int firstElem, LinkedList<Integer> lList){
+        this.firstElem = firstElem;
+        this.lList = lList;
+    }
 
+}
 //Реализуйте очередь с помощью LinkedList со следующими методами:
 // enqueue() - помещает элемент в конец очереди,
 // dequeue() - возвращает первый элемент из очереди и удаляет его,
@@ -11,8 +19,9 @@ public class EX_2 {
         System.out.println(ll);
         LinkedList<Integer> eq = enqueue(ll);
         System.out.println(eq);
-        int dq = dequeue(ll);
-        System.out.println(dq);
+        DequeueL dq = dequeue(ll);
+        System.out.println(dq.firstElem);
+        System.out.println(dq.lList);
         int fst = first(ll);
         System.out.println(fst);
     }
@@ -21,9 +30,9 @@ public class EX_2 {
         lst.add(el);
         return lst;
     }
-    public static int dequeue(LinkedList<Integer> lst){
+    public static DequeueL dequeue(LinkedList<Integer> lst){
         int el = lst.pollFirst();
-        return el;
+        return new DequeueL(el, lst);
     }
     public static int first (LinkedList<Integer> lst){
         int el = lst.peekFirst();
--- src/HW_4/EX_4.java
+++ src/HW_4/EX_4.java
@@ -2,8 +2,15 @@ package HW_4;
 
 import java.util.Random;
 
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
+final class Dequeue {
+    public int firstElem;
+    public int[] array;
+
+    public Dequeue(int firstElem, int[] array) {
+        this.firstElem = firstElem;
+        this.array = array;
+    }
+}
 
 public class EX_4 {
     public static void main(String[] args) {
@@ -60,12 +67,4 @@ public class EX_4 {
     }
 }
 
-final class Dequeue {
-    public int firstElem;
-    public int[] array;
 
-    public Dequeue(int firstElem, int[] array) {
-        this.firstElem = firstElem;
-        this.array = array;
-    }
-}
